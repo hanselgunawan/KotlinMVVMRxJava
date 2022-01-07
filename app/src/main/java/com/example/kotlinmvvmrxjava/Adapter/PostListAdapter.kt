911991ea0a820b2557e8610c24a2b486271fb09c
Hansel Tritama <hansel.tritama@nortonlifelock.com>
2022-01-07 13:59:55 -0800

    Adding adapter and connect it with RxJava call

---
+++ app/src/main/java/com/example/kotlinmvvmrxjava/Adapter/PostListAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.kotlinmvvmrxjava.Adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlinmvvmrxjava.Model.PostModel
+import com.example.kotlinmvvmrxjava.R
+import java.lang.StringBuilder
+
+class PostListAdapter(var context: Context, var postModelList: MutableList<PostModel>) : RecyclerView.Adapter<PostListAdapter.MyViewHolder> {
+
+    inner class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var txtUserId: TextView
+        var txtBody: TextView
+        var txtTitle: TextView
+
+        init {
+            txtUserId = itemView.findViewById(R.id.txtUserId)
+            txtBody = itemView.findViewById(R.id.txtBody)
+            txtTitle = itemView.findViewById(R.id.txtTitle)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(
+            LayoutInflater.from(context).inflate(R.layout.layout_post_item, parent, false)
+        )
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.txtTitle.text = postModelList[position].title.toString()
+        holder.txtBody.text = StringBuilder(postModelList[position].body!!.substring(0, 20))
+            .append("...").toString()
+        holder.txtUserId.text = postModelList[position].userId.toString()
+    }
+
+    override fun getItemCount(): Int {
+        return postModelList.size
+    }
+
+}
--- app/src/main/java/com/example/kotlinmvvmrxjava/MainActivity.kt
+++ app/src/main/java/com/example/kotlinmvvmrxjava/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.kotlinmvvmrxjava
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.app.AlertDialog
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlinmvvmrxjava.Adapter.PostListAdapter
+import com.example.kotlinmvvmrxjava.ViewModel.MainActivityViewModel
+import dmax.dialog.SpotsDialog
 
 class MainActivity : AppCompatActivity() {
+
+    var mainActivityViewModel: MainActivityViewModel? = null
+
+    var recyclerPost: RecyclerView? = null
+    var adapter: PostListAdapter? = null
+    var layoutManager: LinearLayoutManager? = null
+
+    var dialog: AlertDialog? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainActivityViewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
+
+        dialog = SpotsDialog.Builder().setContext(this).setCancelable(false).build()
+        dialog!!.show()
+
+        recyclerPost = findViewById(R.id.recyclerPost)
+        layoutManager = LinearLayoutManager(this)
+        recyclerPost!!.layoutManager = layoutManager
+
+        mainActivityViewModel!!.getPostList.observe(this, { postModels ->
+            Log.e("MainActivity", "PostList: " + postModels.get(0).userId)
+
+            adapter = PostListAdapter(this, postModels)
+            adapter!!.notifyDataSetChanged()
+
+            recyclerPost!!.adapter = adapter
+
+            dialog!!.dismiss()
+        })
     }
 }
